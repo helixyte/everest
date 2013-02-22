7f9cfe20a6d559decdc67edb20347b848d139d6d
F. Oliver Gathmann <fogathmann@gmail.com>
2013-02-22 10:32:58 +0100

    Removed accidentally committed nosql repository code.

--- everest/repositories/nosqldb/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-This file is part of the everest project. 
-See LICENSE.txt for licensing, CONTRIBUTORS.txt for contributor information.
-
-Created on Jan 11, 2013.
-"""
-from .aggregate import NoSqlAggregate as Aggregate
-from .repository import NoSqlRepository as Repository
