helixyte/everest

commit 9cc48f99dff42871237051b90cdabab90c13bd30

Fixed generation of location headers and representations for modifying resource views in cases where IDs were missing because of delayed flushing in the backend.

everest/views/base.py
@@ -30,6 +30,7 @@ from everest.mime import get_registered_mime_type_for_string
 from everest.representers.utils import UpdatedRepresenterConfigurationContext
 from everest.representers.utils import as_representer
 from everest.resources.system import UserMessageMember
+from everest.resources.utils import provides_member_resource
 from everest.resources.utils import resource_to_url
 from everest.url import UrlPartsConverter
 from everest.utils import get_traceback
@@ -235,16 +236,23 @@ class RepresentingResourceView(ResourceView): # still abstract pylint: disable=W
           passed on to a custom renderer).
         """
         if self._convert_response:
-            rpr = self._get_response_representer()
-            # Set content type and body of the response.
-            self.request.response.content_type = \
-                                    rpr.content_type.mime_type_string
-            rpr_body = rpr.to_bytes(resource)
-            self.request.response.body = rpr_body
+            self._update_response_body(resource)
             result = self.request.response
         else:
             result = dict(context=resource)
         return result
+
+    def _update_response_body(self, resource):
+        rpr = self._get_response_representer()
+        # Set content type and body of the response.
+        self.request.response.content_type = \
+                                rpr.content_type.mime_type_string
+        rpr_body = rpr.to_bytes(resource)
+        self.request.response.body = rpr_body
+
+    def _update_response_location_header(self, resource):
+        location = resource_to_url(resource, request=self.request)
+        self.request.response.headerlist.append(('Location', location))
 
     def __get_default_response_mime_type(self):
         if not self._default_response_content_type is None:
@@ -296,7 +304,7 @@ class GetResourceView(RepresentingResourceView): # still abstract pylint: disabl
     def _prepare_resource(self):
         raise NotImplementedError('Abstract method.')
 
-    def _get_result(self, resource):
+    def _update_response_body(self, resource):
         """
         Extends the base class method with links options processing.
         """
@@ -306,10 +314,9 @@ class GetResourceView(RepresentingResourceView): # still abstract pylint: disabl
                                         type(self.context),
                                         self._get_response_mime_type(),
                                         attribute_options=links_options):
-                result = RepresentingResourceView._get_result(self, resource)
+                RepresentingResourceView._update_response_body(self, resource)
         else:
-            result = RepresentingResourceView._get_result(self, resource)
-        return result
+            RepresentingResourceView._update_response_body(self, resource)
 
     def __configure_refs(self):
         refs_options_string = self.request.params.get('refs')
@@ -440,6 +447,31 @@ class ModifyingResourceView(RepresentingResourceView): # still abstract pylint: 
         exception class.
         """
         return '%(code)s %(title)s' % wsgi_http_exc_class.__dict__
+
+    def _get_result(self, resource):
+        """
+        Overrides the base class method to handle the rare case where
+        the URL and representation of the modified resource are not fully
+        available yet (e.g. because the backend has not flushed the changes
+        yet). This is done by deferring setting of the location header
+        (and updating the body, if the response is converted) to a response
+        callback.
+        """
+        if provides_member_resource(resource):
+            loc_rc = resource
+        else:
+            loc_rc = self.context
+        cb_header = lambda request, response: \
+                            self._update_response_location_header(loc_rc)
+        self.request.add_response_callback(cb_header)
+        if self._convert_response:
+            cb_body = lambda request, response: \
+                            self._update_response_body(resource)
+            self.request.add_response_callback(cb_body)
+            result = self.request.response
+        else:
+            result = RepresentingResourceView._get_result(self, resource)
+        return result
 
 
 class PutOrPatchResourceView(ModifyingResourceView):

everest/views/postcollection.py
@@ -6,11 +6,12 @@ See LICENSE.txt for licensing, CONTRIBUTORS.txt for contributor information.
 
 Created on Oct 14, 2011.
 """
+from pyramid.httpexceptions import HTTPCreated
+
 from everest.resources.utils import provides_member_resource
 from everest.resources.utils import provides_resource
-from everest.resources.utils import resource_to_url
 from everest.views.base import ModifyingResourceView
-from pyramid.httpexceptions import HTTPCreated
+
 
 __docformat__ = 'reStructuredText en'
 __all__ = ['PostCollectionView',
@@ -33,8 +34,7 @@ class PostCollectionView(ModifyingResourceView):
             resource = rpr.resource_from_data(data)
         else:
             resource = data
-        member_was_posted = provides_member_resource(resource)
-        if member_was_posted:
+        if provides_member_resource(resource):
             new_members = [resource]
         else:
             new_members = resource
@@ -42,18 +42,12 @@ class PostCollectionView(ModifyingResourceView):
         for new_member in new_members:
             if self.context.get(new_member.__name__) is not None:
                 # We have a member with the same name - 409 Conflict.
-                response = self._handle_conflict(new_member.__name__)
+                result = self._handle_conflict(new_member.__name__)
                 was_created = False
                 break
             else:
                 self.context.add(new_member)
         if was_created:
-            if member_was_posted:
-                new_location = resource_to_url(resource, request=self.request)
-            else:
-                new_location = resource_to_url(self.context,
-                                               request=self.request)
             self.request.response.status = self._status(HTTPCreated)
-            self.request.response.headerlist = [('Location', new_location)]
-            response = self._get_result(resource)
-        return response
+            result = self._get_result(resource)
+        return result

everest/repositories/rdb/session.py
@@ -140,9 +140,11 @@ class RdbSessionFactory(SessionFactory):
                             repository=self._repository)
             if not self._repository.autocommit \
                and self._repository.join_transaction:
-                # Enable the Zope transaction extension with the standard
-                # sqlalchemy Session class.
-                self.__fac.configure(extension=ZopeTransactionExtension())
+                # Enable the Zope transaction extension. We do not want to
+                # throw away the session so that we can still access session
+                # state after the commit (e.g. in a response callback).
+                self.__fac.configure(
+                        extension=ZopeTransactionExtension(keep_session=True))
             else:
                 # Disable extension otherwise.
                 self.__fac.configure(extension=None)

commit cc84f7742ca5a024457b0461856332305a2979eb

Replaced the imperfect solution for the missing ID problem introduced in 9cc48f99dff42871237051b90cdabab90c13bd30 with a better approach.

everest/entities/base.py
@@ -207,6 +207,13 @@ class Aggregate(object):
         """
         raise NotImplementedError('Abstract method.')
 
+    def sync_with_repository(self):
+        """
+        Flushes all pending state to the repository and updates all loaded
+        entities.
+        """
+        raise NotImplementedError('Abstract method.')
+
     @property
     def expression_kind(self):
         """
@@ -403,6 +410,9 @@ class RootAggregate(Aggregate):
     def query(self, **options):
         return self._session.query(self.entity_class, **options)
 
+    def sync_with_repository(self):
+        self._session.flush()
+
     @property
     def expression_kind(self):
         return self._expression_kind
@@ -450,6 +460,9 @@ class RelationshipAggregate(Aggregate):
 
     def query(self):
         return self._root_aggregate.query()
+
+    def sync_with_repository(self):
+        self._root_aggregate.sync_with_repository()
 
     @property
     def entity_class(self):

everest/views/base.py
@@ -243,6 +243,10 @@ class RepresentingResourceView(ResourceView): # still abstract pylint: disable=W
         return result
 
     def _update_response_body(self, resource):
+        """
+        Creates a representer and updates the response body with the byte
+        representation created for the given resource.
+        """
         rpr = self._get_response_representer()
         # Set content type and body of the response.
         self.request.response.content_type = \
@@ -251,8 +255,19 @@ class RepresentingResourceView(ResourceView): # still abstract pylint: disable=W
         self.request.response.body = rpr_body
 
     def _update_response_location_header(self, resource):
+        """
+        Adds a new or replaces an existing Location header to the response
+        headers pointing to the URL of the given resource.
+        """
         location = resource_to_url(resource, request=self.request)
-        self.request.response.headerlist.append(('Location', location))
+        loc_hdr = ('Location', location)
+        hdr_names = [hdr[0].upper() for hdr in self.request.response.headerlist]
+        try:
+            idx = hdr_names.index('LOCATION')
+        except ValueError:
+            self.request.response.headerlist.append(loc_hdr)
+        else:
+            self.request.response.headerlist[idx] = loc_hdr
 
     def __get_default_response_mime_type(self):
         if not self._default_response_content_type is None:

everest/views/postcollection.py
@@ -34,13 +34,18 @@ class PostCollectionView(ModifyingResourceView):
             resource = rpr.resource_from_data(data)
         else:
             resource = data
-        if provides_member_resource(resource):
+        data_is_member = provides_member_resource(resource)
+        if data_is_member:
             new_members = [resource]
         else:
             new_members = resource
         was_created = True
+        sync_with_repo = False
         for new_member in new_members:
-            if self.context.get(new_member.__name__) is not None:
+            name_is_none = new_member.__name__ is None
+            sync_with_repo |= name_is_none
+            if not name_is_none \
+               and not self.context.get(new_member.__name__) is None:
                 # We have a member with the same name - 409 Conflict.
                 result = self._handle_conflict(new_member.__name__)
                 was_created = False
@@ -48,6 +53,17 @@ class PostCollectionView(ModifyingResourceView):
             else:
                 self.context.add(new_member)
         if was_created:
+            if sync_with_repo:
+                # This is not pretty, but necessary: When the resource
+                # name depends on the entity ID, the pending entity needs
+                # to be flushed to the repository before we can access
+                # the ID.
+                self.context.get_aggregate().sync_with_repository()
             self.request.response.status = self._status(HTTPCreated)
+            if data_is_member:
+                loc_rc = resource
+            else:
+                loc_rc = self.context
+            self._update_response_location_header(loc_rc)
             result = self._get_result(resource)
         return result

everest/tests/test_views.py
@@ -40,6 +40,7 @@ from everest.utils import get_repository_manager
 from everest.views.getcollection import GetCollectionView
 from everest.views.static import public_view
 from everest.views.utils import accept_csv_only
+import os
 
 
 __docformat__ = 'reStructuredText en'
@@ -60,7 +61,7 @@ class BasicViewTestCase(FunctionalTestCase):
     ini_file_path = resource_filename('everest.tests.complete_app',
                                       'complete_app.ini')
     app_name = 'complete_app'
-    path = '/my-entities'
+    path = '/my-entities/'
 
     def set_up(self):
         FunctionalTestCase.set_up(self)
@@ -225,6 +226,17 @@ class BasicViewTestCase(FunctionalTestCase):
         coll = get_root_collection(IMyEntity)
         mb = coll['0']
         self.assert_equal(mb.text, 'abc')
+
+    def test_post_collection_no_id(self):
+        req_body = b'"text","number"\n"abc",2\n'
+        res = self.app.post("%s" % self.path,
+                            params=req_body,
+                            content_type=CsvMime.mime_type_string,
+                            status=201)
+        self.assert_is_not_none(res)
+        self.assert_true(res.headers['Location'].endswith(self.path))
+        self.assert_not_equal(native_(res.body).split(os.linesep)[1][:2],
+                              '""')
 
     def test_post_nested_collection(self):
         mb, mb_url = self.__make_parent_and_link()