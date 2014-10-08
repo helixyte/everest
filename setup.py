fbabee48e34e9a6605f6a02ffbca422a17888a2e
F. Oliver Gathmann <fogathmann@gmail.com>
2014-10-08 14:04:38 +0200

    Moving from distribute back to setuptools.

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ Package setup file.
 Created on Nov 3, 2011.
 """
 import os
-from distribute_setup import use_setuptools
-use_setuptools()
+import ez_setup
+ez_setup.use_setuptools()
 from setuptools import setup
 from setuptools import find_packages
 
